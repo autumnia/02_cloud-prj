7e61a879afb479d9b918e362b7ff41af04a36da2
Autumn Kim <autumnya@gmail.com>
2021-09-15 08:48:37 +0900

    수정

--- rabbitmq-service/docker-rabbitmq.cmd
+++ rabbitmq-service/docker-rabbitmq.cmd
@@ -1,3 +1,8 @@
+# 네트워크 확인
+# $ sudo docker network ls 
+# 아래 포트는 방화벽에서 열려 있어야 한다.
+# 기본계정은 admin 
+
 docker run -it --name rabbitmq -h rabbitmq \
 -p 15672:15672 -p 5672:5672 -p 15671:15671 -p 5671:5671 -p 4369:4369 \ 
 -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest \
