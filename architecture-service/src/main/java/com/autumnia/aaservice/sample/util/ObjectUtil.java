92e4225e84da24bc57a0f2bf10645a962bc0734c
Autumn Kim <autumnya@gmail.com>
2022-01-08 23:04:34 +0900

    추가

--- architecture-service/src/main/java/com/autumnia/aaservice/sample/util/ObjectUtil.java
+++ architecture-service/src/main/java/com/autumnia/aaservice/sample/util/ObjectUtil.java
@@ -5,8 +5,33 @@ import org.modelmapper.ModelMapper;
 
 public class ObjectUtil {
 	public static <T> T toModel( Object object,  Class<T> type) {
-		ModelMapper mapper = new ModelMapper();
-		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		return mapper.map(object, type);
+		try {
+			ModelMapper mapper = new ModelMapper();
+			mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+			return mapper.map(object, type);
+		} catch ( Exception e ) {
+			throw new RuntimeException(e);
+		}
 	}
+
+    public static Payload toPayload(Object o){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] bytes = objectMapper.writeValueAsBytes(o);
+            return DefaultPayload.create(bytes);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T toObject(Payload payload, Class<T> type){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            byte[] bytes = payload.getData().array();
+            return objectMapper.readValue(bytes, type);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
