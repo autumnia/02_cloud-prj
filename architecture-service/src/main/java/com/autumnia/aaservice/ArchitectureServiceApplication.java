66ad42dbf63a2d6db1ecf043e04ec74610d4377d
김가을 <autumnya@gmail.com>
2022-01-20 15:54:37 +0900

    임포트 추가

--- architecture-service/src/main/java/com/autumnia/aaservice/ArchitectureServiceApplication.java
+++ architecture-service/src/main/java/com/autumnia/aaservice/ArchitectureServiceApplication.java
@@ -1,7 +1,13 @@
 package com.autumnia.aaservice;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class ArchitectureServiceApplication {
