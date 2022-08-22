04ca77023847b900bdaada4d8c2d8838fd6acfc3
Autumn Kim <autumnya@gmail.com>
2022-08-22 13:30:08 +0900

    유틸 수정

--- architecture-service/src/main/java/com/autumnia/aaservice/sample/util/ObjectUtil.java
+++ architecture-service/src/main/java/com/autumnia/aaservice/sample/util/ObjectUtil.java
@@ -4,15 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class ObjectUtil {
-//	public static <T> T toModel( Object object,  Class<T> type) {
-//		try {
-//			ModelMapper mapper = new ModelMapper();
-//			mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-//			return mapper.map(object, type);
-//		} catch ( Exception e ) {
-//			throw new RuntimeException(e);
-//		}
-//	}
 
 //    public static Payload toPayload(Object o){
 //        try{
