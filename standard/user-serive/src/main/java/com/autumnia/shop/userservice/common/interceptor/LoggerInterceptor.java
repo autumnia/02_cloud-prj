80550de4a92196458f9546b0c55f7bbae81f2756
Autumn Kim <autumnya@gmail.com>
2022-09-01 16:54:05 +0900

    수정

--- standard/user-serive/src/main/java/com/autumnia/shop/userservice/common/interceptor/LoggerInterceptor.java
+++ standard/user-serive/src/main/java/com/autumnia/shop/userservice/common/interceptor/LoggerInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
+
 public class LoggerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
